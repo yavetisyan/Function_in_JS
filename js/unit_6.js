b1ed49cf9f707d06331d13cc337a8e3527e95d46
yavetisyan <mr.avetisyan@yahoo.com>
2021-07-01 21:50:06 +0400

    unit 6 add

--- js/unit_6.js
+++ js/unit_6.js
@@ -12,7 +12,11 @@ function f1() {
 let btn1 = document.querySelector(".btn1");
 let btn2 = document.querySelector(".btn2");
 let btn3 = document.querySelector(".btn3");
+let btn4 = document.querySelector(".btn4");
 let out1 = document.querySelector(".out-1");
+let out2 = document.querySelector(".out-2");
+let out3 = document.querySelector(".out-3");
+let out4 = document.querySelector(".out-4");
 
 //btn1.addEventListener("click", f1);
 
@@ -52,5 +56,20 @@ function sum1(a, b) {
 }
 
 btn2.addEventListener("click", () => {
-  sum1.call(btn2, 9, 4);
+  sum1.call(out1, 9, 4);
+  sum1.apply(out2, [15, 9]);
 });
+
+// bind
+
+const f4 = f1.bind(out3);
+
+f4();
+f4();
+f4();
+
+btn4.addEventListener("click", f4);
+
+const sum2 = sum1.bind(out4);
+sum2(4, 5);
+sum2(4, 15);
